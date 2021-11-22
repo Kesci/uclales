463e1d2c85dabcdfbd3dabd73a894fe6d2c974a1
WidgetA <421413836@qq.com>
2021-11-22 14:16:39 +0800

    update

--- config/ubuntu.cmake
+++ config/ubuntu.cmake
@@ -8,8 +8,8 @@ set(USER_Fortran_FLAGS_DEBUG "-W -Wall -Wuninitialized -fcheck=all -fbacktrace -
 
 set(NETCDF_INCLUDE_DIR "/usr/include")
 set(NETCDF_LIB_1       "/usr/lib/x86_64-linux-gnu/libnetcdff.a")
-set(NETCDF_LIB_2       "/home/ubuntu/netcdf/lib/libnetcdf.a")
+set(NETCDF_LIB_2       "/usr/lib/x86_64-linux-gnu/libnetcdf.so")
 set(HDF5_LIB_1         "/usr/lib/x86_64-linux-gnu/hdf5/serial/libhdf5_hl.a")
-set(HDF5_LIB_2         "/usr/lib/x86_64-linux-gnu/hdf5/serial/libhdf5.a")
+set(HDF5_LIB_2         "/usr/lib/x86_64-linux-gnu/hdf5/serial/libhdf5.so")
 set(SZIP_LIB           "")
 set(LIBS ${NETCDF_LIB_1} ${NETCDF_LIB_2} ${HDF5_LIB_1} ${HDF5_LIB_2} ${SZIP_LIB} m z curl)
